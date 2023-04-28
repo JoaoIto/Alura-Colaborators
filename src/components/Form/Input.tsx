e67918d5c525cc7c3848f1a37b1cd62f98e81f06
João <joaovictorpfr@gmail.com>
2023-04-27 21:26:09 -0300

    html input element

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,27 +1,29 @@
+import React from "react";
+
 interface InputProps {
-    onChanges: (value: string) => void
-    placeholder: string, 
-    label: string, 
-    required: boolean, 
-    value: string
+  label: string;
+  value: string;
+  onChanges: (value: string) => void;
+  required?: boolean;
+  type: string;
+  placeholder: string;
 }
 
-export function Input(props: InputProps){
+export function Input(props: InputProps) {
+  const typing = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChanges(event.target.value);
+  };
 
-    const typing = (event) => {
-        props.onChanges(event.target.value)
-    }
-
-    return(
-        <div className="container">
-            <label>{props.label}</label>
-            <input 
-            value={props.value} 
-            onChange={typing} 
-            required={props.required} 
-            type={props.type} 
-            placeholder={props.placeholder}
-            />
-        </div>
-    )
+  return (
+    <div className="container">
+      <label>{props.label}</label>
+      <input
+        value={props.value}
+        onChange={typing}
+        required={props.required}
+        type={props.type || "text"}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
 }
